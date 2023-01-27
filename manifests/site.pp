70b667336a0efbdda32e2e93f4bb8d2570e25a56
asrivastava-github <35009943+asrivastava-github@users.noreply.github.com>
2023-01-27 15:55:05 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node default {
+  file { '/root/README':
+    ensure  => file,
+    content => 'Thi sis jsaf reead,e',
+    owner   => 'root'
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
